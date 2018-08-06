be9db3d2f82ffa06ee712fee8af1ac612b72cdb2
Noah Peeters <noah.peeters@icloud.com>
2018-08-06 17:11:56 +0200

    Add filter and sorting to ics

--- Sources/App/Controllers/MealController.swift
+++ Sources/App/Controllers/MealController.swift
@@ -17,8 +17,21 @@ final class MealController {
     }
 
     func icsIndex(_ req: Request) throws -> Future<String> {
+        let sortVegeterian = try? req.query.get(Int.self, at: "sortVegeterian")
+        let filterVegeterian = try? req.query.get(Int.self, at: "filterVegeterian")
+
         return Meal.query(on: req).all()
             .map(to: String.self) { meals in
+                var meals = meals
+                if filterVegeterian == 0 {
+                    meals = meals.filter { !$0.vegetarian }
+                } else if filterVegeterian == 1 {
+                    meals = meals.filter { $0.vegetarian }
+                } else if sortVegeterian == 0 {
+                    meals = meals.sorted { meal, _ in !meal.vegetarian }
+                } else if sortVegeterian == 1 {
+                    meals = meals.sorted { meal, _ in meal.vegetarian }
+                }
                 return meals.reduce(icsHeader) { $0 + $1.toICSEvent() } + icsFooter
             }
     }
